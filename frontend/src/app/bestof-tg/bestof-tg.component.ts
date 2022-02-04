a520c3ac6a28ca6b299467d8a9d9ac2583d395e0
Mansi <singhrk.mansi@gmail.com>
2022-02-04 10:00:30 -0500

    Home page and view post component uploaded

---
+++ frontend/src/app/bestof-tg/bestof-tg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bestof-tg',
+  templateUrl: './bestof-tg.component.html',
+  styleUrls: ['./bestof-tg.component.css']
+})
+export class BestofTgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/logobar/logobar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogobarComponent } from './logobar.component';
+
+describe('LogobarComponent', () => {
+  let component: LogobarComponent;
+  let fixture: ComponentFixture<LogobarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogobarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogobarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
