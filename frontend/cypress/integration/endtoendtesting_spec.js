7bc38d0feb5ef6f98c38eff7bc5ebf3710899f4d
Mansi Singh <singhrk.mansi@gmail.com>
2022-03-04 22:28:28 -0500

    Cypress test cases for end to end user testing

--- frontend/cypress/integration/endtoendtesting_spec.js
+++ frontend/cypress/integration/endtoendtesting_spec.js
@@ -5,36 +5,19 @@ describe("Test 1", function(){
 })
 
 describe("Test 2", function(){
-    it("Finds the login button", function(){
+    it("Login Form working", function(){
         cy.contains('Login').click()
     })
 })
 
 describe("Test 3", function(){
-    it("Finds the signup button", function(){
+    it("Signup form working", function(){
         cy.contains('Signup').click()
     })
 })
 
 
-
-describe("Test 11", function(){
-    it("Find description in create post form is  functional", function(){
-        cy.contains('Description').click({force: true})
-       
-    })
-
-})
-
-describe("Test 12", function(){
-    it("Find address in create post form is  functional", function(){
-        cy.contains('Address').click({force: true})
-       
-    })
-
-})
-
-describe("Test 13", function(){
+describe("Test 4", function(){
     it("Find checkbox in create post form is  functional", function(){
         cy.contains('Show my personal information').click({force: true})
        
@@ -43,50 +26,42 @@ describe("Test 13", function(){
 })
 
 
-describe("Test 4", function(){
+describe("Test 5", function(){
     it("Checks if navbar is  functional", function(){
         cy.contains('Community').click()
-       
-    })
-})
-describe("Test 5", function(){
-    it("Checks if navbar is functional", function(){
         cy.contains('Housing').click( {force: true})
-       
-    })
-
-})
-describe("Test 6", function(){
-    it("Checks if navbar is  functional", function(){
         cy.contains('Gigs').click({force: true})
-       
-    })
-
-})
-describe("Test 7", function(){
-    it("Checks if navbar is functional", function(){
         cy.contains('Jobs').click({force: true})
        
     })
-
 })
-describe("Test 8", function(){
-    it("Checks if navbar is  functional", function(){
+
+
+describe("Test 6", function(){
+    it("Checks if navbar Services is  functional", function(){
         cy.contains('Services').click({force: true})
        
     })
 
 })
-describe("Test 9", function(){
-    it("Checks if navbar is  functional", function(){
+describe("Test 7", function(){
+    it("Checks if navbar Forums is  functional", function(){
         cy.contains('Forums').click({force: true})
        
     })
 
 })
-describe("Test 10", function(){
-    it("Checks if navbar is  functional", function(){
+describe("Test 8", function(){
+    it("Checks if navbar For Sale is  functional", function(){
         cy.contains('For Sale').click({force: true})
+       
+    })
+
+})
+
+describe("Test 9", function(){
+    it("Checks if Grid component navigation is functional", function(){
+        cy.contains('Household').click({force: true})
        
     })
 
