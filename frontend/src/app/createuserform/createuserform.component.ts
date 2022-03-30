a73f8a5891bc85ac4b79d48db11307dc4b770101
Mansi <singhrk.mansi@gmail.com>
2022-03-30 16:37:14 -0400

    Form Validation for login and create user form

--- frontend/src/app/createuserform/createuserform.component.ts
+++ frontend/src/app/createuserform/createuserform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-createuserform',
   templateUrl: './createuserform.component.html',
@@ -14,6 +15,20 @@ export class CreateuserformComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  form = new FormGroup({
+    firstname: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    lastname: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
+  get f(){
+    return this.form.controls;
+  }
+  
+  submit(){
+    console.log(this.form.value);
+  }
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `${result}`;
--- frontend/src/app/loginform/loginform.component.ts
+++ frontend/src/app/loginform/loginform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
@@ -8,11 +9,23 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 export class LoginformComponent implements OnInit {
   closeResult = '';
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal) {}                          
 
   ngOnInit(): void {
   }
+  
+  form = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
+  get f(){
+    return this.form.controls;
+  }
+  
+  submit(){
+    console.log(this.form.value);
+  }
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `${result}`;
