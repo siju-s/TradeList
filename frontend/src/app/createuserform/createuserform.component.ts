f706953c0a587f1b95dbe568e21ff5d19cb0207f
Mansi <singhrk.mansi@gmail.com>
2022-03-29 21:17:17 -0400

    Login Signup form modal display

--- frontend/src/app/createuserform/createuserform.component.ts
+++ frontend/src/app/createuserform/createuserform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-createuserform',
   templateUrl: './createuserform.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateuserformComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {
   }
 
-}
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `${result}`;
+    }, (reason) => {
+      this.closeResult = `${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return '';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return '';
+    } else {
+      return ``;
+    }
+  }
+} 
--- frontend/src/app/loginform/loginform.component.ts
+++ frontend/src/app/loginform/loginform.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
   styleUrls: ['./loginform.component.css']
 })
 export class LoginformComponent implements OnInit {
+  closeResult = '';
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {
   }
 
-  
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `${result}`;
+    }, (reason) => {
+      this.closeResult = `${this.getDismissReason(reason)}`;
+    });
+  }
 
-}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return '';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return '';
+    } else {
+      return ``;
+    }
+  }
+} 
+
