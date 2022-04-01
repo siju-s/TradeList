3e5dfc338fbc7661b8de64fc1268948e2f18b7c0
Mansi <singhrk.mansi@gmail.com>
2022-04-01 14:33:55 -0400

    End to end test cases for login sign up form

--- frontend/cypress/integration/endtoendtesting_spec.js
+++ frontend/cypress/integration/endtoendtesting_spec.js
@@ -1,69 +1,50 @@
-describe("Test 1", function(){
+describe("Find the index page", function(){
     it("Finds the index page", function(){
         cy.visit('http://localhost:4200/')
     })
 })
 
-describe("Test 2", function(){
+describe("Find the login form", function(){
     it("Login Form working", function(){
         cy.contains('Login').click()
     })
 })
 
-describe("Test 3", function(){
+describe("Fill the login form", function(){
+ it('Interacting with text fields',()=>{
+    cy.get('input[id="email"]')
+      .type('test@gmail.com')
+ .should('have.value','test@gmail.com')
+ .get('input[id="password"]')
+ .type('12345678')
+ .should('have.value','12345678')
+ .get('button[id="submit"]').click()
+ .get('button[id="close"]').click()
+ })
+})
+
+describe("Find the signup form", function(){
     it("Signup form working", function(){
-        cy.contains('Signup').click()
+        cy.contains('Sign up').click()
     })
 })
 
 
-describe("Test 4", function(){
-    it("Find checkbox in create post form is  functional", function(){
-        cy.contains('Show my personal information').click({force: true})
-       
+describe("Fill the signup form", function(){
+    it('Interacting with text fields',()=>{
+       cy.get('input[id="firstname"]')
+         .type('Mark')
+    .should('have.value','Mark')
+    .get('input[id="lastname"]')
+    .type('Jacobs')
+    .should('have.value','Jacobs')
+    .get('input[id="useremail"]')
+      .type('test@gmail.com')
+ .should('have.value','test@gmail.com')
+ .get('input[id="userpassword"]')
+ .type('12345678')
+ .should('have.value','12345678')
+    .get('button[id="signupsubmit"]').click()
+    .get('button[id="signupclose"]').click()
     })
-
-})
-
-
-describe("Test 5", function(){
-    it("Checks if navbar is  functional", function(){
-        cy.contains('Community').click()
-        cy.contains('Housing').click( {force: true})
-        cy.contains('Gigs').click({force: true})
-        cy.contains('Jobs').click({force: true})
-       
-    })
-})
-
-
-describe("Test 6", function(){
-    it("Checks if navbar Services is  functional", function(){
-        cy.contains('Services').click({force: true})
-       
-    })
-
-})
-describe("Test 7", function(){
-    it("Checks if navbar Forums is  functional", function(){
-        cy.contains('Forums').click({force: true})
-       
-    })
-
-})
-describe("Test 8", function(){
-    it("Checks if navbar For Sale is  functional", function(){
-        cy.contains('For Sale').click({force: true})
-       
-    })
-
-})
-
-describe("Test 9", function(){
-    it("Checks if Grid component navigation is functional", function(){
-        cy.contains('Household').click({force: true})
-       
-    })
-
-})
-
+   })
