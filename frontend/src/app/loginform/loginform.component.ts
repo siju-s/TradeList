dc1a2509c5519b03b2a5314996419a6a74942db2
Siju S <sijus887@gmail.com>
2022-04-01 22:39:22 -0400

    Connect Login frontend to db

--- frontend/src/app/loginform/loginform.component.ts
+++ frontend/src/app/loginform/loginform.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Login, LoginService} from "../services/login.service";
+
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
@@ -8,24 +10,44 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 })
 export class LoginformComponent implements OnInit {
   closeResult = '';
+  isLoginFailed = false
+  errorMessage = ''
+  isFormSubmitted = false
 
-  constructor(private modalService: NgbModal) {}                          
+  constructor(private modalService: NgbModal, private loginService: LoginService) {
+  }
 
   ngOnInit(): void {
   }
-  
+
   form = new FormGroup({
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     email: new FormControl('', [Validators.required, Validators.email])
   });
 
-  get f(){
+  get f() {
     return this.form.controls;
   }
-  
-  submit(){
+
+  submit() {
+    this.isFormSubmitted = true
     console.log(this.form.value);
+    const login: Login = {
+      Email: this.form.value.email,
+      Password: this.form.value.password
+    }
+    this.loginService.login(login).subscribe(data => {
+      console.log(data)
+      const status = data["status"]
+      this.isLoginFailed = status == 404;
+      this.errorMessage = data["message"]
+      this.form.reset()
+      if (!this.isLoginFailed) {
+        this.modalService.dismissAll()
+      }
+    })
   }
+
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `${result}`;
@@ -43,5 +65,5 @@ export class LoginformComponent implements OnInit {
       return ``;
     }
   }
-} 
+}
 
---
+++ frontend/src/app/services/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {JobPost} from "../post.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  userFound : Boolean
+  constructor(private http: HttpClient) {
+  }
+
+  login(login: Login) : Observable<any> {
+    return this.http.post<any>(environment.gateway + '/login', JSON.stringify(login))
+  }
+
+}
+
+export interface Login {
+  Email :string;
+  Password :string;
+}
