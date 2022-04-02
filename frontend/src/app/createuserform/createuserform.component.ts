8820fe8b6656a06e4e08eb25fd7a595769f85311
Siju S <sijus887@gmail.com>
2022-04-01 23:23:28 -0400

    Connect Signup frontend to backend

--- frontend/src/app/createuserform/createuserform.component.ts
+++ frontend/src/app/createuserform/createuserform.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import {SignupService, User} from "../services/signup.service";
 @Component({
   selector: 'app-createuserform',
   templateUrl: './createuserform.component.html',
@@ -9,8 +10,11 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 export class CreateuserformComponent implements OnInit {
 
   closeResult = '';
+  isSignupFailed = false
+  errorMessage = ''
+  isFormSubmitted = false
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private signupService: SignupService) {}
 
   ngOnInit(): void {
   }
@@ -25,9 +29,27 @@ export class CreateuserformComponent implements OnInit {
   get f(){
     return this.form.controls;
   }
-  
+
   submit(){
     console.log(this.form.value);
+    const user: User = {
+      Contact: {
+        FirstName: this.form.value.firstname,
+        LastName: this.form.value.lastname,
+        Email: this.form.value.email,
+        Password: this.form.value.password
+      }
+    }
+    this.signupService.signup(user).subscribe(data => {
+      console.log(data)
+      const status = data["status"]
+      this.isSignupFailed = status == 0;
+      this.errorMessage = data["message"]
+      this.form.reset()
+      if (!this.isSignupFailed) {
+        this.modalService.dismissAll()
+      }
+    })
   }
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
@@ -46,4 +68,4 @@ export class CreateuserformComponent implements OnInit {
       return ``;
     }
   }
-} 
+}
