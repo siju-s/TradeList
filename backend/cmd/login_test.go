691c6f818eb7ca6047a755187984ac01f0feba79
yashincontrol <yashincontrol@gmail.com>
2022-04-18 19:51:41 -0400

    create login test file

---
+++ backend/cmd/login_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSignup(test *testing.T) {
+	repo := repoMock{}
+
+}
+
+func TestLogin(test *testing.T) {
+	repo := repoMock{}
+
+}
+
+func TestLogout(test *testing.T) {
+	repo := repoMock{}
+
+}
+
+func TestForgotPassword(test *testing.T) {
+	repo := repoMock{}
+
+}
